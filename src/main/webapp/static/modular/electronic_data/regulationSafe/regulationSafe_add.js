586f420d230e2883fc126c50e1fd451cf427a68e
lm <lm@163.com>
2020-02-27 22:32:32 +0800

    fix:解决先上传附件再提交表单问题

--- src/main/webapp/static/modular/electronic_data/regulationSafe/regulationSafe_add.js
+++ src/main/webapp/static/modular/electronic_data/regulationSafe/regulationSafe_add.js
@@ -15,6 +15,8 @@ layui.use(['layer', 'form', 'admin', 'ax', 'upload'], function () {
     var layer = layui.layer;
     var upload = layui.upload;
     var fileStatus = false;
+    var ajax;
+    var formData;
 
     // 让当前iframe弹层高度适应
     // admin.iframeAuto(); 影响选择结构树
@@ -80,44 +82,24 @@ layui.use(['layer', 'form', 'admin', 'ax', 'upload'], function () {
         //
         // }
         , done: function (res) {
-            console.log(res);
-            // if (res.CODE === 200) {
-            //     fileStatus= true;
-            //     alert("aaaa");
-            //     var formSatellite = document.getElementById("regulationSafeForm");//获取所要提交form的id
-            //     var data = new FormData(formSatellite);  //用所要提交form做参数建立一个formdata对象
-            //     fsubmit(data);//调用函数
-            // //     var tr = demoListView.find('tr#upload-' + index)
-            // //         , tds = tr.children();
-            // //     tds.eq(2).html('<span style="color: #5FB878;">上传成功</span>');
-            // //     tds.eq(3).html(''); //清空操作
-            // //
-            // //     name += res.fileName + ",";
-            // //     $("#enclosure").val(name);
-            // }
-        }
-        , error: function () {
-            console.log("异常")
+            if (res.CODE === 200) {
+                ajax = new $ax(Feng.ctxPath + "/regulationSafe/add", function (data) {
+                    if (data.success) {
+                        Feng.success("增加成功!");
+                        admin.putTempData('formOk', true);//传给上个页面，刷新table用
+                        admin.closeThisDialog();//关掉对话框
+                    } else {
+                        Feng.error(data.message);
+                    }
+                }, function (data) {
+                    Feng.error("增加失败!" + data.message)
+                });
+                ajax.set(formData);
+                ajax.start();
+            }
         }
     });
 
-    // function fsubmit(data) {
-    //     $.ajax({
-    //         url: Feng.ctxPath + "/regulationSafe/add",
-    //         type: "POST",
-    //         data: date,
-    //         async: false,
-    //         contentType: false,   //jax 中 contentType 设置为 false 是为了避免 JQuery 对其操作，从而失去分界符，而使服务器不能正常解析文件
-    //         processData: false,   //当设置为true的时候,jquery ajax 提交的时候不会序列化 data，而是直接使用data
-    //         error: function (request) {
-    //             parent.layer.alert("网络超时");
-    //         },
-    //         success: function (data) {
-    //             alert("上传成功！");
-    //         }
-    //     });
-    //     return false;
-    // }
 
     function selectOnlyByName(data) {
         $.ajax({
@@ -132,25 +114,10 @@ layui.use(['layer', 'form', 'admin', 'ax', 'upload'], function () {
 
     // 表单提交事件
     form.on('submit(btnSubmit)', function (data) {
-        // console.log(uploadInst.upload());
+        formData = data.field;
+        console.log(formData);
         uploadInst.upload();
-        if (fileStatus){
-            alert("aaa");
-            var ajax = new $ax(Feng.ctxPath + "/regulationSafe/add", function (data) {
-                if (data.success) {
-                    Feng.success("增加成功!");
-                    admin.putTempData('formOk', true);//传给上个页面，刷新table用
-                    admin.closeThisDialog();//关掉对话框
-                } else {
-                    Feng.error(data.message);
-                }
-            }, function (data) {
-                Feng.error("增加失败!" + data.message)
-            });
-            ajax.set(data.field);
-            ajax.start();
-            return false;
-        }
+        return false
 
     });
 });

5a3d86e9dd149e49e778945c9daf5eadecea70f6
zhaizl2009@163.com <zhaizl2009@163.com>
2020-03-02 13:51:42 +0800

    fix: 公用代码 - 时间格式化

--- src/main/java/com/seven/gwc/core/base/GwcBaseEntity.java
+++ src/main/java/com/seven/gwc/core/base/GwcBaseEntity.java
@@ -1,5 +1,6 @@
 package com.seven.gwc.core.base;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -19,16 +20,18 @@ public class GwcBaseEntity extends BaseEntity{
     /**
      * 更新时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     protected Date updateDate;
     /**
      * 更新人ID
      */
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     protected String updatePerson;
     /**
      * 创建时间
      */
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     protected Date createDate;
     /**
      * 创建人ID

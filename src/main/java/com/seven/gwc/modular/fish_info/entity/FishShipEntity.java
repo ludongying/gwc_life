b0ac41f6370f2bc9b6488bac567d139ba894f6ca
shenhaoqian@hetaitf.com <qian960911>
2020-03-16 18:03:08 +0800

    fix：修改渔船信息实体

--- src/main/java/com/seven/gwc/modular/fish_info/entity/FishShipEntity.java
+++ src/main/java/com/seven/gwc/modular/fish_info/entity/FishShipEntity.java
@@ -31,32 +31,33 @@ public class FishShipEntity implements Serializable {
     @TableId(value = "id", type = IdType.UUID)
     private String id;
 
-    /** 名称 */
-    private String name;
-
-    /** 管理类别-字典 */
-    private String areaType;
-    @TableField(exist=false)
-    private String areaTypeName;
-
-    /** 编码 */
+    /** 渔船编码 */
     private String code;
+
+    /** 船名 */
+    private String name;
 
     /** 船籍 */
     private String registry;
 
+    //水域分类
+    private String watersType;
+    @TableField(exist=false)
+    private String watersTypeName;
+
+    /** 区域分类-字典 */
+    private String areaType;
+    @TableField(exist=false)
+    private String areaTypeName;
+
     /** 船舶类型-字典 */
     private String shipType;
+
     @TableField(exist=false)
     private String shipTypeName;
 
     /** 船体材质 */
     private String hullMaterial;
-
-    /** 作业类型-字典 */
-    private String workType;
-    @TableField(exist=false)
-    private String workTypeName;
 
     /** 船长度 */
     private Double shipLong;
@@ -67,11 +68,21 @@ public class FishShipEntity implements Serializable {
     /** 船深 */
     private Double shipDeep;
 
+    /** 总吨位 */
+    private String tonnage;
+
     /** 主机总功率 */
     private Double totalPower;
 
-    /** 吨位 */
-    private String tonnage;
+    /** 作业类型-字典 */
+    private String workType;
+    @TableField(exist=false)
+    private String workTypeName;
+
+    //作业方式
+    private String practice;
+    @TableField(exist=false)
+    private String practiceName;
 
     /** 建成日期 */
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -84,11 +95,19 @@ public class FishShipEntity implements Serializable {
     /** 证件号码 */
     private String identity;
 
-    /** 手机号码 */
-    private String phone;
-
-    /** 地址 */
+    /** 船主地址 */
     private String address;
+
+    //核定航区
+    private String approvedArea;
+
+    //核定成员
+    private String authorizedMember;
+
+    /** 文件名 */
+    private String fileName;
+
+
 
     /** 出海状态 */
     private String seaState;
@@ -97,9 +116,6 @@ public class FishShipEntity implements Serializable {
 
     /** 是否重点 */
     private Boolean keyPoints;
-
-    /** 文件名 */
-    private String fileName;
 
     /** 路径 */
     private String filePath;

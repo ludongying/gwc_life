838a7d0479db19b26a069b1e913a1b341172c218
shenhaoqian <shenhaoqian@tonfun.cn>
2020-03-19 11:31:24 +0800

    feat：电子档案详情

--- src/main/java/com/seven/gwc/modular/lawrecord/controller/ElectronicController.java
+++ src/main/java/com/seven/gwc/modular/lawrecord/controller/ElectronicController.java
@@ -14,16 +14,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("electronic")
-public class electronicController {
+public class ElectronicController {
     private static String PREFIX = "/modular/electronicFile/";
 
     @Autowired
     private ElectronicFileService electronicFileService;
+    @Autowired
+    private LawRecordService lawRecordService;
 
     @RequestMapping("")
     public String index( Model model) {
         model.addAttribute("lawTypes", LawTypeEnum.values());
         return PREFIX + "electronicFile";
+    }
+
+    @RequestMapping("/detail")
+    public String detail(String id, Model model) {
+        lawRecordService.detail(id,model);
+        return PREFIX + "electronicFile_detail";
     }
 
     /**

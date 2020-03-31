f948c2a3221daf86ce61da472b654f366745f5bf
zhaizl2009@163.com <zhaizl2009@163.com>
2020-04-01 00:05:35 +0800

    feat: 增加详情点击效果

--- src/main/webapp/static/modular/lawRecord/detail.js
+++ src/main/webapp/static/modular/lawRecord/detail.js
@@ -7,14 +7,15 @@ layui.use(['layer', 'form', 'admin', 'ax'], function () {
     var $ = layui.jquery;
     $(".layui-form input").attr("disabled","disabled");
 
-    var fileManager=new initFiles($);
-    $("#evidence .demo-down-1").click(function () {
-           fileManager.downLoad($(this).data("filePath"));
-    });
-    
-    $("#evidence .demo-preview-1").click(function () {
-        fileManager.preview_img($(this).data("filePath"));
-    });
+    // var fileManager=new initFiles($);
+    // $("#evidence .demo-down-1").click(function () {
+    //        fileManager.downLoad($(this).data("filePath"));
+    // });
+    //
+    // $("#evidence .demo-preview-1").click(function () {
+    //     fileManager.preview_img($(this).data("filePath"));
+    // });
+
     layer.open({
         title:'目录',
         type: 1,
@@ -23,7 +24,13 @@ layui.use(['layer', 'form', 'admin', 'ax'], function () {
         shade:false,
         skin:'layui-layer-dir',
         resize:false,
-        content: $("#anchor_id").html()
+        content: $("#anchor_id").html(),
+
+    });
+
+    $(document).on("click",".site-dir li",function () {
+        $(".site-dir li").removeClass("bold_font");
+        $(this).addClass("bold_font");
     });
 
 
